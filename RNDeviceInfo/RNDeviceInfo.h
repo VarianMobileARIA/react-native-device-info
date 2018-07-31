1abe55f2bbe5dc7bdf8dbcd3c56d4c6068f25061
Sushant Sardeshpande <ssardeshpande@elivotech.com>
2018-07-31 11:01:20 +0530

    Update RNDeviceInfo.h

--- RNDeviceInfo/RNDeviceInfo.h
+++ RNDeviceInfo/RNDeviceInfo.h
@@ -9,11 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <sys/utsname.h>
 
-#if __has_include(<React/RCTAssert.h>)
 #import <React/RCTBridgeModule.h>
-#else
-#import "RCTBridgeModule.h"
-#endif
 
 @interface RNDeviceInfo : NSObject <RCTBridgeModule>
 
